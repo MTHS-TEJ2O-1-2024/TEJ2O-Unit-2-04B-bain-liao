d7320a62d16dfa1cea65e8d0b358d01ed92f1757
bain-liao <liao.bain@mths.ca>
2024-09-19 11:23:07 -0400

    updated comment, title, and added temperature finder

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Sep 2024
+ * This program finds out the temperature
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pause(1000)
+
+let findTemperature = input.temperature()
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("The temperature is:")
+    basic.showNumber(findTemperature)
+    basic.showString("C.")
+    basic.showIcon(IconNames.Happy)
+})
