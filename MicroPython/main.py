f7bc059b0fb319622b5971c9961bc36ae51a5560
bain-liao <liao.bain@mths.ca>
2024-09-20 14:53:49 +0000

    made temperature code in python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Bain Liao
+Created on: Sep 2024
+This module figures out the temperature
 """
 
 from microbit import *
+from time import sleep
 
+display.clear()
+display.show(Image.HAPPY)
+sleep(1)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        find_temperature = input.temperature()
+        display.clear()
+        display.show("The temperature is:")
+        display.show(str(find_temperature))
+        display.show("C.")
+        display.show(Image.HAPPY)
