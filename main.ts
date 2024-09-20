69b8ae5090a3d934c34c34b7812d188ad253cf16
bain-liao <liao.bain@mths.ca>
2024-09-19 20:11:13 -0400

    fixed code to say real temperature instead of 21 everytime

--- main.ts
+++ main.ts
@@ -2,16 +2,15 @@
  *
  * Created by: Bain Liao
  * Created on: Sep 2024
- * This program finds out the temperature
+ * This program finds out the temperature.
 */
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 pause(1000)
 
-let findTemperature = input.temperature()
-
 input.onButtonPressed(Button.A, function () {
+    let findTemperature = input.temperature()
     basic.clearScreen()
     basic.showString("The temperature is:")
     basic.showNumber(findTemperature)
